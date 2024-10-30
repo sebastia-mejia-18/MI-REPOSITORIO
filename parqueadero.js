be479ac2215d0469b8b183d0bf40c342c39ce410
sebastian mejia <juansebastianmejiaagudelo73@gmail.com>
2024-10-30 12:41:03 -0500

    se agrega funcion de inisio de sesion para el parqueader

---
+++ parqueadero.js
@@ -0,0 +1,8 @@
+// Función de inicio de sesión
+function iniciarSesion() {
+    const usuario = prompt("Ingrese su nombre de usuario:");
+    const contraseña = prompt("Ingrese su contraseña:");
+    // Aquí podrías implementar la validación de usuario y contraseña si es necesario
+    console.log("Inicio de sesión exitoso.");
+}
+
