eec863f1f81e339fbcc8bc582416d5a4470a5d5b
sebastian mejia <juansebastianmejiaagudelo73@gmail.com>
2024-10-30 12:45:16 -0500

    Descripción del cambio realizado

--- parqueadero.js
+++ parqueadero.js
@@ -6,3 +6,22 @@ function iniciarSesion() {
     console.log("Inicio de sesión exitoso.");
 }
 
+// Función para calcular el costo de estacionamiento
+function calcularCosto(entrada, salida) {
+    const horaEntrada = new Date(`1970-01-01T${entrada}:00`);
+    const horaSalida = new Date(`1970-01-01T${salida}:00`);
+
+    const diferenciaHoras = (horaSalida - horaEntrada) / (1000 * 60 * 60);
+    let costo;
+
+    if (diferenciaHoras <= 2) {
+        costo = diferenciaHoras * 3000;
+    } else if (diferenciaHoras <= 5) {
+        costo = diferenciaHoras * 2500;
+    } else {
+        costo = diferenciaHoras * 2000;
+    }
+
+    return costo;
+}
+
